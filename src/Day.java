4003d66b09f7918145a48d3a2785c4391fa0b1e7
BT_1E10_10 <eleanorc25@nycstudents.net>
2022-06-07 12:15:53 -0400

    Initial commit

---
+++ src/Day.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Day {
+    public static void main(String[] args) {
+        Restaurant r = new Restaurant();
+        System.out.println("");
+        r.runDay();
+
+
+
+    }
+}
---
+++ src/Table.java
@@ -0,0 +1,14 @@
+public class Table {
+    private int people;
+    public Table(int p){
+        people = p;
+    }
+
+    public void setPeople(int people) {
+        this.people = people;
+    }
+
+    public int getPeople() {
+        return people;
+    }
+}
